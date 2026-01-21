5d5166b5e5903cead7436b6dd59c67abd5904127
Shivani-268 <shivani.connect8@gmail.com>
2026-01-21 12:41:17 +0530

    Add files via upload

---
+++ rules.js
@@ -0,0 +1,32 @@
+export const darkPatterns = [
+  {
+    name: "Guilt Tripping",
+    keywords: [
+      "no thanks",
+      "i don't care",
+      "i hate",
+      "miss out",
+      "lose benefits"
+    ],
+    weight: 3
+  },
+  {
+    name: "Forced Consent",
+    keywords: [
+      "by continuing",
+      "you agree",
+      "cannot proceed",
+      "required"
+    ],
+    weight: 4
+  },
+  {
+    name: "Misdirection",
+    keywords: [
+      "accept all",
+      "agree & continue",
+      "allow all"
+    ],
+    weight: 2
+  }
+];
